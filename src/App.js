28b0bb2f35af7503c722e81997921fe86d549858
Nikolay <areyfclm@gmail.com>
2023-06-08 23:36:35 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [text, setText] = useState('')
+
+  const addTodo = () => {
+    if (text.trim().length) {
+      setTodos([
+        ...todos,
+        {
+          id: new Date().toISOString(),
+          text,
+          completed: false
+        }
+      ])
+      setText('')
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        <input value={text} onChange={(e) => setText(e.target.value)}></input>
+        <button onClick={addTodo}>Add to do</button>
+      </label>
+
+      <ul className='todo-list'>
+        {todos.map(todo => <li key={todo.id}>
+          <input type="checkbox" />
+          <span>{todo.text}</span>
+          <span>&times;</span>
+        </li>)}
+      </ul>
     </div>
   );
 }
